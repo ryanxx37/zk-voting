8c99699027f25490f32d08e706b560f6a1238323
ryanxx37 <ryanxx37@gmail.com>
2026-04-21 11:14:13 -0400

    Update heading and description in Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,8 @@
     <div class="row justify-content-md-center">
       <div class="col-lg-4">
         <div class="text-center vstack gap-3">
-          <h1>Simple voting system</h1>
+          <h1>ZK voting system</h1>
+          <p class="text-muted">Phase 2: Blockchain Voting</p>
           <a href="#/registration" class="btn btn-primary"
             >Registration to vote</a> 
           <a href="#/validator" class="btn btn-primary">Validator tool</a>
